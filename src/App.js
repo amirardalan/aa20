251b4e52bfa706c497020abc6dd3ae14f585ac61
Amir Ardalan <amir@Amirs-MacBook-Pro.local>
2020-12-17 10:23:30 -0800

    style App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
+import logo from './folder.svg';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="folder" />
+        <h1 className="App-title">
+          Amir Ardalan
+        </h1>
         <p>
           UI Designer & Front-end Developer
         </p>
-        <a
-          className="App-link"
+        <button
+          className="App-button"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          View Work
+        </button>
       </header>
     </div>
   );
