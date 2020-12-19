61a257805fc92e92a3d95add57b67860ddc785f7
Amir Ardalan <amirrardalan@gmail.com>
2020-12-19 00:28:16 -0800

    update page styles

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Work.scss'
+import logo from '../logo.svg'
 
 function Work() {
     return (
         <div className="Work Page">
-            <h1>Work</h1>
+            <div class="Page-heading">
+                <img src={logo} className="Page-logo" alt="logo" />
+                <h1>Work</h1>
+            </div>
         </div>
     )
 }
