2b8e3996f9d46f6e265aaac18686c91712d9bc04
Amir Ardalan <amirrardalan@gmail.com>
2020-12-22 12:07:30 -0800

    update home h1 styling and cleanup sass

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import './Work.scss'
 import logo from '../img/logo.svg'
 import ReactGA from 'react-ga'
 
@@ -33,22 +32,22 @@ function Work() {
                 <div className="Page-content">
                     <div className="Content-item">
                         <h3>Hanna<br/> Andersson</h3>
-                        <strong>Agency Consultant &mdash; 2019</strong>
+                        <strong>Agency Consultant {'—'} 2019</strong>
                         <p>Contributed code for several featured shopping experiences on Hanna Andersson's ecommerce site. Consulted internal management and graphic designers on design and development in Salesforce Commerce Cloud.</p>
                     </div>
                     <div className="Content-item">
                         <h3>KEEN<br/> Footwear</h3>
-                        <strong>LEAD UI ENGINEER &mdash; 2015-2018</strong>
+                        <strong>LEAD UI ENGINEER {'—'} 2015-2018</strong>
                         <p>Collaborated with <a className="Text-link" href="https://www.basicagency.com/" target="_blank" rel="noreferrer" onClick={BasicAgencyLink}>BASIC&reg; Agency</a> to deliver a Salesforce Commerce Cloud adaptive ecommerce experience for KEEN. Localized content and shopping functionality for increased international sales and visiblity.</p>
                     </div>
                     <div className="Content-item">
                         <h3>CHROME<br/> Industries</h3>
-                        <strong>LEAD UI ENGINEER &mdash; 2015-2018</strong>
+                        <strong>LEAD UI ENGINEER {'—'} 2015-2018</strong>
                         <p>Contributed to a brand-specific UI/UX experience on a SFCC instance shared with KEEN Footwear. Utilized reusable front-end modules and strict adherance to a living styleguide.</p>
                     </div>
                     <div className="Content-item">
                         <h3>Columbia<br/> Sportswear</h3>
-                        <strong>Web Developer I &mdash; 2014-2015</strong>
+                        <strong>Web Developer I {'—'} 2014-2015</strong>
                         <p>Worked directly with internal Graphic Design, UX and Product Owner teams to deliver responsive, front-end features for Columbia Sportswear's SFCC ecommerce platform. Developed the 2014 Performance Fishing Gear landing page.</p>
                     </div>
                 </div>
