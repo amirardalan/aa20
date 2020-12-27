5bb7640f966d4d33341e4672748aad1f983a7789
Amir Ardalan <amirrardalan@gmail.com>
2020-12-27 02:59:38 -0800

    add download resume functionality

--- src/App.js
+++ src/App.js
@@ -19,7 +19,14 @@ function App() {
         category: 'Link',
         action: 'Footer Home Link'
     })
-}
+  }
+
+  const DownloadResumeButton = () => {
+    ReactGA.event({
+        category: 'Button',
+        action: 'Download Resume Button'
+    })
+  }
 
   return (
     <div className="App">
@@ -31,9 +38,13 @@ function App() {
           <Route path="/bio" component={Bio} />
         </Switch>
         <footer>
-          Copyright &copy;
-          <span>{(new Date().getFullYear())}</span>
-          &mdash; <Link to="/" onClick={FooterHomeLink} className="Text-link">Amir Ardalan</Link>
+          <a className="Text-link Mobile-only" href="/amir-ardalan-resume.pdf" onClick={DownloadResumeButton} aria-label="Download resume"> Download Resume {'⤓'}</a>
+          <p>
+            Copyright &copy;
+            <span>{(new Date().getFullYear())}</span>
+            {'–'} <Link to="/" onClick={FooterHomeLink} className="Text-link">Amir Ardalan</Link>
+          </p>
+          <a className="Button-secondary" href="/amir-ardalan-resume.pdf" onClick={DownloadResumeButton} target="_blank" rel="noreferrer" aria-label="Download resume"> Download Resume {'⤓'}</a>
         </footer>
       </Router>
     </div>
